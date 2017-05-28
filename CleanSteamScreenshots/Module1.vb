dc2352d046e7eadc1713d400e4f33bb3b3867c5b
Adam <Adam@AirborneGamer.com>
2017-05-27 22:04:52 -0500

    Full VB.NET code
    Console application

---
+++ CleanSteamScreenshots/Module1.vb
@@ -0,0 +1,35 @@
+﻿Imports System.IO
+
+Module Module1
+
+    Sub Main()
+        CleanSteamScreenshots()
+    End Sub
+
+    Private Sub CleanSteamScreenshots()
+
+        Dim steamScreenshotDirectory As String = "C:\Program Files (x86)\Steam\userdata\6223619\760\remote"
+        Dim di As New DirectoryInfo(steamScreenshotDirectory)
+
+        For Each folder In di.GetDirectories
+            For Each subFolder In folder.GetDirectories
+                If subFolder.Name.ToUpper = "SCREENSHOTS" Then
+                    DeleteScreenshots(subFolder)
+                End If
+            Next
+        Next
+    End Sub
+
+    Private Sub DeleteScreenshots(subFolder As DirectoryInfo)
+
+        For Each item In subFolder.GetDirectories
+            Directory.Delete(item.FullName, True)
+        Next
+
+        For Each item In subFolder.GetFiles
+            item.Delete()
+        Next
+
+    End Sub
+
+End Module
---
+++ CleanSteamScreenshots/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("CleanSteamScreenshots")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("CleanSteamScreenshots")> 
+<Assembly: AssemblyCopyright("Copyright ©  2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("67478577-77c4-4f36-b568-0d251e6e2f87")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
